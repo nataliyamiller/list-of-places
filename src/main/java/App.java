1fa9337f60ce814ee2641c89aef21ae641c4f9d8
Guest User <student@epicodus.com>
2015-08-18 11:17:27 -0700

    Tests passed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,5 +17,23 @@ public class App {
     return new ModelAndView(model, layout);
   }, new VelocityTemplateEngine());
 
+  post("/restaurants", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    ArrayList<Restaurants> restaurants = request.session().attribute("restaurants");
+
+    if(restaurants == null) {
+      restaurants = new ArrayList<Restaurants>();
+      request.session().attribute("restaurants", restaurants);
+    }
+
+    String description = request.queryParams("description");
+    Restaurants newRestaurants = new Restaurants(description);
+
+    restaurants.add(newRestaurants);
+
+    model.put("template", "templates/success.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
  }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -23,8 +23,23 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
     goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("Add a new restaurant:");
+    assertThat(pageSource()).contains("Restaurants List");
   }
-  
+
+  @Test
+  public void restaurantIsCreated() {
+    goTo("http://localhost:4567/");
+    fill("#description").with("Panera");
+    submit(".btn");
+    assertThat(pageSource()).contains("Success!");
   }
-}
+
+  @Test
+  public void restaurantIsDisplayedTest() {
+    goTo("http://localhost:4567/");
+    fill("#description").with("Panera");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    assertThat(pageSource()).contains("Panera");
+  }
+  }
