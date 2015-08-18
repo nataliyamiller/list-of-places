356fb7c598993e8c4cc5976b1568a82960e4bff3
Guest User <student@epicodus.com>
2015-08-18 10:35:31 -0700

    Initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,23 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import java.util.ArrayList;
+
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  
